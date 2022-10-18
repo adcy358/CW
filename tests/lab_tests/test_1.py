24c4f19366cfdd6e9e0553f5ab4f3fef1b30c68f
Erik <erikgutierrezduthiers@MacBook-Pro-de-Erik.local>
2022-10-18 13:32:30 +0100

    First attempt at pytest

---
+++ tests/lab_tests/test_1.py
@@ -0,0 +1,4 @@
+# pytest is both the library name and the command
+# when you run the command, it looks for files starting with the name "test"
+# and runs any function within the file starting with the name "test"
+# Pytest uses the Assert keyboard: anything it evaluates to false means the test failed.
