95a14d7e60ea28c093efed6aac972a60454a77a9
Erik <erikgutierrezduthiers@MacBook-Pro-de-Erik.local>
2022-10-28 19:35:50 +0100

    Generate grid 1st attempt

---
+++ src/Task1/Implementation.py
@@ -0,0 +1,36 @@
+#Implementation of the game
+import numpy as np 
+
+class Grid: 
+
+    def __init__(self, length, width): 
+        
+        self.length = length
+        self.width = width
+
+    def generate_grid(self, number_random_states=42):
+
+        #we generate n=42 methods for generating random numbers
+        rng = np.random.RandomState(number_random_states)
+
+        #we create our matrix grid (Length x Width)
+        grid = np.zeros((self.length, self.width))
+
+        for i in range(self.length): 
+            for j in range(self.width): 
+                random_number = rng.random_integers(0,9)
+                grid[i][j] = random_number
+
+        return grid 
+
+    def plot(self): 
+        
+        grid = self.generate_grid()
+        
+        return(print(grid))
+
+grid = Grid(3,3)
+grid.plot()
+
+
+
---
+++ src/Task1/Strategies.py
@@ -0,0 +1,2 @@
+from Implementation import Grid
+
