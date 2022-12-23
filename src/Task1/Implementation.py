fb022469fc9f4eb8e9cbba26c3dc4f9bc5f2cff9
Erik <erikgutierrezduthiers@MBP-de-Erik.home>
2022-12-23 18:57:47 +0100

    main.py created

--- src/Task1/Implementation.py
+++ src/Task1/Implementation.py
@@ -8,11 +8,11 @@ class Grid:
         self.length = length
         self.width = width
 
-    def generate_random_grid(self, number_random_states=42):
+    def generate_random_grid(self, seed=42):
         # this method generates a random grid of dimensions length x width
 
         #we generate n=42 methods for generating random numbers
-        rng = np.random.RandomState(number_random_states)
+        rng = np.random.RandomState(seed)
 
         #we create our matrix grid (Length x Width)
         grid = np.zeros((self.length, self.width))
@@ -36,20 +36,3 @@ class Grid:
                 index += 1
 
         return grid
-
-#game = Grid(4, 3)
-
-#rg = game.generate_random_grid(3)
-#cg = game.generate_custom_grid(8, 3, 8, 8, 0, 5, 3, 5, 7, 6, 0, 4)
-
-#print(rg)
-#print(cg)
-
-'''
-TODO: 
-- Fix generate_grid()
-    It always generates the same grid for values (width, length)
-
-    
-'''
-
---
+++ src/Task1/main.py
@@ -0,0 +1,25 @@
+from Strategies import *
+
+game = Shortest_Path(5, 5)
+
+#generate grid
+grid = game.generate_random_grid(3)
+print(grid)
+
+
+#dijkstra's algorithm
+shortest_path, timestep, previous_nodes, shortest_path_grid = game.dijkstra_algorithm(grid)
+print('Shortest path: ')
+print(shortest_path)
+print('')
+print('Previous nodes:')
+print(previous_nodes)
+print('')
+print('Timestep: ')
+print(timestep)
+
+
+#heuristic algorithm
+shortest_path, timestep  = game.heuristic_algorithm(grid)
+print(shortest_path)
+print('heuristic: ', timestep)
